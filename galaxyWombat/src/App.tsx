d2c73b345f0b69cd73598aa083c31630405548f6
Radosław Marek <dotims054@gmail.com>
2024-10-05 22:15:44 +0200

    galaxy view appropriate version

--- galaxyWombat/src/App.tsx
+++ galaxyWombat/src/App.tsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SpaceGameMain from './SubPages/SpaceGame/spaceGameMain'
+import SpaceGameMain from './SubPages/SpaceGame/SpaceGameMain';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <div>
