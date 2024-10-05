a4c4de8d73ff7475e4bcf3067def13c4e968c101
Radosław Marek <dotims054@gmail.com>
2024-10-05 15:11:52 +0200

    three.js config + changes on solar file

---
+++ galaxyWombat/src/SubPages/SpaceGame/SpaceGameMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpaceGameMain = () => {
+  return (
+    <section>
+        <iframe src="/solar.html" width="1400px" height="850px"></iframe>
+    </section>
+  )
+}
+
+export default SpaceGameMain
