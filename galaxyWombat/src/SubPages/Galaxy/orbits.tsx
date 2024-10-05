83c753e9085e7d305a6afc7535f0e30abbb6a099
Dawid Buszta <53567837+Dawidsjd@users.noreply.github.com>
2024-10-05 17:28:55 +0200

    adding slonce

--- galaxyWombat/src/SubPages/Galaxy/orbits.tsx
+++ galaxyWombat/src/SubPages/Galaxy/orbits.tsx
@@ -18,7 +18,7 @@ export const planetData: PlanetData[] = [
     rho: 0.05,
     size: 0.383,
     color: 'gray',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/mercury.jpg', 
     speed: 0.0235, // 0.047 decreased by 50%
     description:
       'Mercury is the closest planet to the Sun and the smallest planet in the Solar System.',
@@ -30,7 +30,7 @@ export const planetData: PlanetData[] = [
     rho: 0.1,
     size: 0.949,
     color: 'yellow',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/venus.jpg', 
     speed: 0.0175, // 0.035 decreased by 50%
     description:
       'Venus is the second planet from the Sun and is similar in structure to Earth.',
@@ -42,7 +42,7 @@ export const planetData: PlanetData[] = [
     rho: 0.15,
     size: 1.0,
     color: 'blue',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/earth.jpg', 
     speed: 0.015, // 0.03 decreased by 50%
     description:
       'Earth is the third planet from the Sun and the only astronomical object known to support life.',
@@ -54,7 +54,7 @@ export const planetData: PlanetData[] = [
     rho: 0.2,
     size: 0.532,
     color: 'red',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/mars.jpg', 
     speed: 0.012, // 0.024 decreased by 50%
     description:
       'Mars is the fourth planet from the Sun and is often called the "Red Planet".',
@@ -66,7 +66,7 @@ export const planetData: PlanetData[] = [
     rho: 0.5,
     size: 11.21,
     color: 'orange',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/jupiter.jpg', 
     speed: 0.0065, // 0.013 decreased by 50%
     description:
       'Jupiter is the fifth planet from the Sun and the largest planet in the Solar System.',
@@ -78,7 +78,7 @@ export const planetData: PlanetData[] = [
     rho: 0.9,
     size: 9.45,
     color: 'lightyellow',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/saturn.jpg', 
     speed: 0.0045, // 0.009 decreased by 50%
     description:
       'Saturn is the sixth planet from the Sun and is known for its stunning rings.',
@@ -90,7 +90,7 @@ export const planetData: PlanetData[] = [
     rho: 1.3,
     size: 4.01,
     color: 'lightblue',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/uranus.jpg', 
     speed: 0.0035, // 0.007 decreased by 50%
     description:
       'Uranus is the seventh planet from the Sun and is unique for its sideways rotation.',
@@ -102,7 +102,7 @@ export const planetData: PlanetData[] = [
     rho: 1.7,
     size: 3.88,
     color: 'darkblue',
-    texture: '/assets/sun_texture.jpg', 
+    texture: '/assets/neptune.jpg', 
     speed: 0.0025, // 0.005 decreased by 50%
     description:
       'Neptune is the eighth and farthest planet from the Sun in the Solar System.',
