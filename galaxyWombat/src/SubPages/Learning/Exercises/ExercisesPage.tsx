c812663da5b7d1e43ccc94bc6203539153040c0c
bezoski <rafalbuczkowski2004@wp.pl>
2024-10-06 09:42:15 +0200

    change back btn style in learning

--- galaxyWombat/src/SubPages/Learning/Exercises/ExercisesPage.tsx
+++ galaxyWombat/src/SubPages/Learning/Exercises/ExercisesPage.tsx
@@ -22,7 +22,7 @@ const ExercisesPage = () => {
       <div className="absolute top-6 left-6">
         <Link
           to="/learning"
-          className=" text-white text-lg rounded-full border-2 border-gray-700 border-b-[6px] m-2.5 inline-flex items-center justify-center px-6 py-3 transition-all duration-100 hover:bg-gray-800 hover:border-gray-600 active:border-b-2 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 active:translate-y-[4px] opacity-65 hover:opacity-100 z-50"
+          className="flex items-center px-4 py-2  text-white rounded-lg opacity-70 hover:opacity-100"
         >
           <FaArrowLeft className="mr-2" /> Back
         </Link>
