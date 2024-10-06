94f05858fff5bbbf2934d5fbb3085c1648e1843e
Radosław Marek <dotims054@gmail.com>
2024-10-06 08:41:52 +0200

    learning quiz questions for planets

--- galaxyWombat/src/SubPages/Learning/Exercises/ExercisesPage.tsx
+++ galaxyWombat/src/SubPages/Learning/Exercises/ExercisesPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';  // Hook do pobierania stanu z nawigacji
 import { FaBook, FaPowerOff, FaWind, FaSun, FaDumbbell, FaOpencart } from 'react-icons/fa';
 import DuolingoButton from './ui/DuolingoButton';
+import { Link } from 'react-router-dom';
 
 const ExercisesPage = () => {
   const location = useLocation();  // Pobieranie stanu przekazanego z nawigacji
@@ -17,32 +18,43 @@ const ExercisesPage = () => {
           <div className="mb-10">
             <img src={planet.image} alt={planet.label} className="absolute top-1/3 left-[15%] w-96 h-auto object-contain" />
             <h2 className="absolute top-1/2 right-[15%] text-2xl mt-4 font-bold text-center tracking-wider z-10 text-white" style={{
-          letterSpacing: '8px',
-          textShadow: '2px 2px 5px #3b82f6',
-          fontSize: '5rem',
-        }}>{planet.label}</h2>
-            
+              letterSpacing: '8px',
+              textShadow: '2px 2px 5px #3b82f6',
+              fontSize: '5rem',
+            }}>{planet.label}</h2>
           </div>
         )}
 
         <div>
           <div>
-            <DuolingoButton icon={<FaPowerOff size={32} className="text-white" />} />
+            <Link to="/learning/exercise/techQuiz" state={{ planet }}>
+              <DuolingoButton icon={<FaPowerOff size={32} className="text-white" />} />
+            </Link>
           </div>
           <div className="ml-32">
-            <DuolingoButton icon={<FaBook size={32} className="text-white" />} />
+            <Link to="/book-quiz" state={{ planet }}>
+              <DuolingoButton icon={<FaBook size={32} className="text-white" />} />
+            </Link>
           </div>
           <div className="ml-64">
-            <DuolingoButton icon={<FaWind size={32} className="text-white" />} />
+            <Link to="/wind-quiz" state={{ planet }}>
+              <DuolingoButton icon={<FaWind size={32} className="text-white" />} />
+            </Link>
           </div>
           <div className="ml-40">
-            <DuolingoButton icon={<FaSun size={32} className="text-white" />} />
+            <Link to="/sun-quiz" state={{ planet }}>
+              <DuolingoButton icon={<FaSun size={32} className="text-white" />} />
+            </Link>
           </div>
           <div className="ml-8">
-            <DuolingoButton icon={<FaDumbbell size={32} className="text-white" />} />
+            <Link to="/dumbbell-quiz" state={{ planet }}>
+              <DuolingoButton icon={<FaDumbbell size={32} className="text-white" />} />
+            </Link>
           </div>
           <div className="-ml-24">
-            <DuolingoButton icon={<FaOpencart size={32} className="text-white" />} />
+            <Link to="/cart-quiz" state={{ planet }}>
+              <DuolingoButton icon={<FaOpencart size={32} className="text-white" />} />
+            </Link>
           </div>
         </div>
       </div>
---
+++ galaxyWombat/src/SubPages/Learning/Questions/Quiz2step/BookQuiz.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookQuiz = () => {
+  return (
+    <div>BookQuiz</div>
+  )
+}
+
+export default BookQuiz
