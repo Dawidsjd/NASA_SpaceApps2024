15d0539bc5055da9748726d89f05493d242bf3da
Radosław Marek <dotims054@gmail.com>
2024-10-06 02:14:56 +0200

    fix bug with redirection and reload page

--- galaxyWombat/src/SubPages/SpaceGame/SpaceGameMain.tsx
+++ galaxyWombat/src/SubPages/SpaceGame/SpaceGameMain.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const SpaceGameMain = () => {
   return (
-    <section className='bg-[#000042]'>
-        <iframe src="/solar.html" width="1400px" height="850px"></iframe>
+    <section className='h-[100vh]'>
+        <iframe src="/solar.html" width="100%" className='!h-[100vh]'></iframe>
     </section>
   )
 }
 
-export default SpaceGameMain
+export default SpaceGameMain;
